a210b808d345d4537f80dbdb8e4007a44bb185c5
diwinigah <winidorc1@gmail.com>
2026-06-17 06:50:00 +0000

    ajout

--- app/Filament/Resources/SponsoringResource/Pages/EditSponsoring.php
+++ app/Filament/Resources/SponsoringResource/Pages/EditSponsoring.php
@@ -21,7 +21,42 @@ class EditSponsoring extends EditRecord
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
         /** @var CampEdition $record */
-        return app(CampEditionService::class)->updateEdition($record, $data);
+        // Liste blanche des champs sponsoring uniquement
+        $sponsoringFields = [
+            'show_sponsoring_page',
+            'sponsoring_theme',
+            'sponsoring_intro',
+            'sponsoring_verse',
+            'budget_total',
+            'budget_collected',
+            'participants_target',
+            'participants_sponsored',
+            'bourse_pleine_amount',
+            'bourse_adulte_amount',
+            'bourse_etudiant_amount',
+            'bourse_lycee_amount',
+            'bourse_enfant_amount',
+            'payment_flooz',
+            'payment_mixx',
+            'payment_iban',
+            'payment_paypal',
+            'payment_account_name',
+            'payment_account_number',
+            'sponsoring_contact_phone',
+            'sponsoring_contact_email',
+            'nature_contributions',
+        ];
+
+        // Utiliser le service si des transformations supplémentaires existent
+        if (app()->bound(CampEditionService::class)) {
+            // Déléguer à la méthode de service si elle attend tout le tableau
+            return app(CampEditionService::class)->updateEdition($record, $data);
+        }
+
+        // Sinon, mise à jour manuelle en ne touchant qu'aux champs sponsoring
+        $record->update(collect($data)->only($sponsoringFields)->toArray());
+
+        return $record;
     }
 
     protected function getHeaderActions(): array
