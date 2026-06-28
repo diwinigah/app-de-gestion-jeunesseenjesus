f11e2cdaa2379383c9a2c1d53733e69389a0d40b
diwinigah <winidorc1@gmail.com>
2026-06-28 11:31:50 +0000

    ajout

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,14 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+    <p style="color:#9ca3af; font-size:13px;">
+        © {{ date('Y') }} <strong>Jeunesse en Jésus</strong><br>
+        <a href="https://jeunesseenjesus.org" style="color:#E8490F;">jeunesseenjesus.org</a>
+    </p>
+</td>
+</tr>
+</table>
+</td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display:inline-block;">
+    <img src="https://jeunesseenjesus.org/images/logo.png" 
+     alt="Jeunesse en Jesus" 
+     style="height:50px; width:auto;">
+</a>
+</td>
+</tr>
